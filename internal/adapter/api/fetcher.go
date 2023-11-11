1b0c01b786202a8097f32e0b592f2f687aaabb44
Kravchuk Roman <kravchuk.roman02@gmail.com>
2023-11-11 22:22:49 +0800

    perf: web api for age, gender, nationality

---
+++ internal/adapter/api/fetcher.go
@@ -0,0 +1,52 @@
+package api
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+)
+
+var ErrEmptyName = errors.New("the name is empty")
+
+func get(ctx context.Context, endpoint, name string) ([]byte, error) {
+	if name == "" {
+		return nil, ErrEmptyName
+	}
+
+	params := url.Values{
+		"name": {name},
+	}
+
+	uri, err := url.Parse(endpoint)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse endpoint: %w", err)
+	}
+
+	uri.RawQuery = params.Encode()
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, uri.String(), http.NoBody)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	client := http.Client{
+		Transport: http.DefaultTransport,
+		Timeout:   http.DefaultClient.Timeout,
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch: %w", err)
+	}
+	defer res.Body.Close()
+
+	success := res.StatusCode >= http.StatusOK && res.StatusCode < http.StatusMultipleChoices
+	if !success {
+		return nil, fmt.Errorf("failed to fetch: %w", err)
+	}
+
+	return io.ReadAll(res.Body)
+}
